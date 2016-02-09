f8c6ef770459eacff50a2cc21b9d331ec8405151
bachtsui <dbtsui2@gmail.com>
2016-02-08 16:05:19 -0800

    Created some seed data

--- js/app.js
+++ js/app.js
@@ -3,5 +3,21 @@ var app = angular.module("movieFun", []);
 app.controller("MovieController", MovieController);
 
 function MovieController(){
-  
+  this.movie =[
+  {
+  	id: 1,
+  	name: "Matrix",
+  	year: 1999 
+  },
+  {
+  	id: 2,
+  	name: "Groundhogs Day",
+  	year: 1993
+  },
+  {
+  	id: 3,
+  	name: "The Social Network",
+  	year: 2010
+  }
+  ];
 }
